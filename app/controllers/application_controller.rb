0928fa32cd4bd37614967458dc165b8a71d24a2e
Sean Floyd <seanfloyd@icloud.com>
2020-06-26 13:07:32 -0400

    Setup shops controller & header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :setup_data_for_menu
+
+  private
+
+  def setup_data_for_menu
+    shop_taxons
+  end
+
+  def shop_taxons
+    @taxons_with_at_least_one_available_shop = Taxon.with_at_least_one_shop.pluck(:name)
+  end
 end
--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class Taxon < ApplicationRecord
   has_and_belongs_to_many :shops
   has_and_belongs_to_many :products
+
+  scope :with_at_least_one_shop, -> { joins(:shops).where('shops.available_on <= ?', Date.today).select(:name).distinct }
 end
